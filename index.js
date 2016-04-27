210b1b56eab08585c3137d1c9a9747c065f9d836
Jonathan Apodaca <jonathan.apodaca@colostate.edu>
2016-04-27 10:04:23 -0600

    Add manifest.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.use('/build', express.static(path.join(__dirname, 'build')))
+app.use('/', express.static(__dirname))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 app.listen(8888, function (err) {
 	if (err) throw err

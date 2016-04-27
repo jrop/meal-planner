7947dac369875bdcfbaac1234490e1ab44618c42
Jonathan Apodaca <jonathan.apodaca@colostate.edu>
2016-04-27 13:47:35 -0600

    Move away from dialogs

--- src/index.js
+++ src/index.js
@@ -12,14 +12,10 @@ import CardText from 'material-ui/lib/card/card-text'
 import ContentAdd from 'material-ui/lib/svg-icons/content/add'
 import FlatButton from 'material-ui/lib/flat-button'
 import FloatingActionButton from 'material-ui/lib/floating-action-button'
-import MenuItem from 'material-ui/lib/menus/menu-item'
-import RaisedButton from 'material-ui/lib/raised-button'
-import SelectField from 'material-ui/lib/select-field'
 import Tabs from 'material-ui/lib/tabs/tabs'
 import Tab from 'material-ui/lib/tabs/tab'
-import TextField from 'material-ui/lib/text-field'
 
-import showDialogForResult from './dialog'
+import MealEditor from './meal-editor'
 
 import _ from 'lodash'
 
@@ -39,13 +35,8 @@ class Meal extends React.Component {
 	}
 
 	async onEditClick() {
-		const component = await showDialogForResult('Edit Meal', <MealEditor meal={this.state.meal} />)
-		if (!component) return
-		
-		const meal = component.state.meal
-		await MEALS.update({ _id: meal._id }, meal, { })
-		this.setState({ meal })
-		this.props.parent.loadData()
+		const app = this.props.parent.props.parent
+		app.push(<Padded><MealEditor parent={app} meal={this.state.meal} meals={MEALS} /></Padded>)
 	}
 
 	render() {
@@ -69,78 +60,6 @@ class Meal extends React.Component {
 	}
 }
 
-class MealEditor extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = { meal: props.meal || {
-			name: 'New Meal',
-			ingredients: [ ],
-			season: 'any',
-		} }
-	}
-
-	addIngredient() {
-		const meal = this.state.meal
-		meal.ingredients.push({ name: `Ingredient ${meal.ingredients.length + 1}`, quantity: '' })
-		this.setState({ meal })
-	}
-
-	removeIngredient(i) {
-		const meal = this.state.meal
-		meal.ingredients.splice(i, 1)
-		this.setState({ meal })
-	}
-
-	updateName() {
-		const meal = this.state.meal
-		meal.name = this.refs.name.getValue()
-		this.setState({ meal })
-	}
-
-	updateIngredientName(i) {
-		const meal = this.state.meal
-		meal.ingredients[i].name = this.refs[`ingredientName${i}`].getValue()
-		this.setState({ meal })
-	}
-
-	updateIngredientQuantity(i) {
-		const meal = this.state.meal
-		meal.ingredients[i].quantity = this.refs[`ingredientQuantity${i}`].getValue()
-		this.setState({ meal })
-	}
-
-	updateSeason(e, i, value) {
-		const meal = this.state.meal
-		meal.season = value
-		this.setState({ meal })
-	}
-
-	render() {
-		return <div>
-			<div><TextField ref="name" hintText="Name" floatingLabelText="Name" value={this.state.meal.name} onChange={this.updateName.bind(this)} /></div>
-			
-			<ul>
-				{this.state.meal.ingredients.map((ingredient, i) => <li key={i}>
-					<TextField ref={`ingredientName${i}`} value={ingredient.name} hintText="Name" floatingLabelText="Name" onChange={this.updateIngredientName.bind(this, i)}/>
-					<TextField ref={`ingredientQuantity${i}`} value={ingredient.quantity} hintText="Quantity" floatingLabelText="Quantity" onChange={this.updateIngredientQuantity.bind(this, i)} />
-					<FlatButton label="Remove" onClick={this.removeIngredient.bind(this, i)} />
-				</li>)}
-				{this.state.meal.ingredients.length ? null : <li><i>No ingredients</i></li>}
-			</ul>
-
-			<div><RaisedButton label="Add Ingredient" onClick={this.addIngredient.bind(this)} /></div>
-
-			<h3>Season</h3>
-			<SelectField ref="season" value={this.state.meal.season} onChange={this.updateSeason.bind(this)}>
-				<MenuItem value="any" primaryText="Any" />
-				<MenuItem value="freezing" primaryText="Freezing" />
-				<MenuItem value="cold" primaryText="Cold" />
-				<MenuItem value="warm" primaryText="Warm" />
-			</SelectField>
-		</div>
-	}
-}
-
 class MealManager extends React.Component {
 	constructor(props) {
 		super(props)
@@ -154,19 +73,10 @@ class MealManager extends React.Component {
 	}
 
 	async addNewMeal() {
-		const component = await showDialogForResult('New Meal', <MealEditor />)
-		if (!component) return
-		
-		const meal = component.state.meal
-		await MEALS.update({ _id: meal._id }, meal, { upsert: true })
-		let meals = this.state.meals
-		meals.push(meal)
-		meals = _.sortBy(meals, 'name')
-		this.setState({ meals })
+		this.props.parent.push(<Padded><MealEditor parent={this.props.parent} meals={MEALS} /></Padded>)
 	}
 
 	render() {
-		console.log(this.state.meals)
 		return <div>
 			{this.state.meals ?
 				this.state.meals.length > 0 ?
@@ -182,9 +92,27 @@ class MealManager extends React.Component {
 	}
 }
 
+const Main = (props) => <Tabs>
+	<Tab label="Meals"><Padded><MealManager parent={props.parent} /></Padded></Tab>
+	<Tab label="Planner"><Padded>TODO</Padded></Tab>
+</Tabs>
+
 class App extends React.Component {
 	constructor(props) {
 		super(props)
+		this.state = { stack: [ <Main parent={this} /> ] }
+	}
+
+	push(screen) {
+		const stack = this.state.stack
+		stack.push(screen)
+		this.setState({ stack })
+	}
+
+	pop() {
+		const stack = this.state.stack
+		stack.splice(stack.length - 1, 1)
+		this.setState({ stack })
 	}
 
 	render() {
@@ -192,18 +120,12 @@ class App extends React.Component {
 			<AppBar
 				title="Meal Planner"
 				showMenuIconButton={false} />
-
-			<Tabs>
-				<Tab label="Meals"><Padded><MealManager /></Padded></Tab>
-				<Tab label="Planner"><Padded>TODO</Padded></Tab>
-			</Tabs>
+			{this.state.stack[this.state.stack.length - 1]}
 		</div>
 	}
 }
 
 window.addEventListener('load', function () {
 	injectTapEventPlugin()
-	const store = nedb({ filename: 'meals', autoload: true })
-	window.store = store
 	ReactDOM.render(<App />, document.getElementById('react'))
 })

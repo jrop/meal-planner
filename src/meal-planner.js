fd759f10caeedd199d846ea9f45206eaa5c44e4a
Jonathan Apodaca <jonathan.apodaca@colostate.edu>
2016-05-03 14:09:55 -0600

    Split out code; updated material-ui

---
+++ src/meal-planner.js
@@ -0,0 +1,8 @@
+import { Frame } from 'react-frame-layout'
+import React from 'react'
+
+export default class MealPlanner extends Frame {
+	render() {
+		return <div>Meal planner - TODO</div>
+	}
+}
---
+++ src/padded.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default (props) => <div style={{ margin: '15px' }}>{props.children}</div>
